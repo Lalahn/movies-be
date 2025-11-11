e6f8f4928165ad45835caf220c67efade1a7c133
Lalahn <lauravalentina200102@gmail.com>
2025-11-10 20:12:41 -0500

    xxx

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("¡Hola! Servidor en funcionamiento.");
+  res.send("¡Hola! Servidor en funcionamiento xxx.");
 });
 
 app.listen(PORT, () => {
