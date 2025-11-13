f3dd143a84451f5e5e575612ba40fc9e4f19c00a
Lalahn <laura-valentina-2001@hotmail.com>
2025-11-13 18:14:42 -0500

    Added get

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,1 +1,22 @@
 const Movie = require("../models/Movie");
+
+// Obtener todas las películas
+exports.getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ message: "Error al listar películas", error });
+  }
+};
+
+// Obtener una película por ID
+exports.getMovieById = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) return res.status(404).json({ message: "Película no encontrada" });
+    res.json(movie);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener película", error });
+  }
+};
